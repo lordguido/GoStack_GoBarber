598bee1a7d81d0e60eda2e01c704e723ff36b274
jonasborgesweb <jonasborgesweb@gmail.com>
2020-04-15 15:46:49 -0300

    Criado a Migration e a Model de Usuario

--- back-end/src/database/migrations/1586976051551-CreateUsers.ts
+++ back-end/src/database/migrations/1586976051551-CreateUsers.ts
@@ -1,26 +1,31 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1586900633747 implements MigrationInterface {
+export class CreateUsers1586976051551 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -38,7 +43,7 @@ export class CreateAppointments1586900633747 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 
 }
